d1d82313272da3f813ae16289f570bf3cfca2f9a
isha-lanjwal <111967698+isha-lanjwal@users.noreply.github.com>
2023-05-03 16:53:37 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalComp from './Component/FunctionalComp';
-import {ClassComp,ClassComp1} from './Component/ClassComp';
+import { ClassComp, ClassComp1 } from './Component/ClassComp';
 import Click from './Component/Click';
 import Counter from './Component/Counter';
 import ParentComp from './Component/ParentComp';
 import ClassProps from './Component/ClassProps';
+import React from 'react';
 
 function App() {
   return (
     <div>
-       <h1>Hello, Welcome to simplilearn.</h1>
-       <h1>This video is about component</h1>
-       <FunctionalComp name="Isha"></FunctionalComp>
-       <ClassComp></ClassComp>
-       <ClassComp1></ClassComp1>
-       <Click></Click>
-       <Counter></Counter>
-       <ParentComp /> 
-       <ClassProps name="Isha" place="Mumbai"><p>Hello paragraph here</p></ClassProps>
-       <ClassProps name="Pratik" place="Mumbai"><button>Click</button></ClassProps>
-       {/* self closing of component */}
+      <h1>Hello, Welcome to simplilearn.</h1>
+      <h1>This video is about component</h1>
+      <FunctionalComp name="Isha"></FunctionalComp>
+      <ClassComp></ClassComp>
+      <ClassComp1></ClassComp1>
+      <Click></Click>
+      <Counter></Counter>
+      <ParentComp />
+      <ClassProps name="Isha" place="Mumbai">
+        <p>Hello paragraph here</p>
+      </ClassProps>
+      <ClassProps name="Pratik" place="Mumbai">
+        <button>Click</button>
+      </ClassProps>
+      {/* self closing of component */}
     </div>
   );
 }
